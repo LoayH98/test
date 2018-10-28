96602ae82e04cba80eb36f4426f09ce2629009f6
Loay <lhamayel2016@gmail.com>
2018-10-28 04:23:03 +0200

    first commit

---
+++ src/Question_1.java
@@ -0,0 +1,7 @@
+public class Question_1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Name:\nLab #:\nDate:\nInstructor's name:\n\n\n\tTasks:");
+	}
+}
--- src/Question_2.java
+++ src/Question_2.java
@@ -3,5 +3,6 @@ public class Question_2
 	public static void main(String[] args)
 	{
 		System.out.print("*Happy Birthday!!*");
+		System.out.print("Loay Hamayel");
 	}
 }
